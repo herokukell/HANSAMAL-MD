05843f8d903eb6ca8c1e02f2afed64b9f3248229
herokukell <herokukell@gmail.com>
2025-02-13 15:49:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? 'HANSAMAL-MD=N1wX0L4S#JQZrUih3JtzS07iPOxBuZ-LhlWSTOG052kOxdgLCp0g' : process.env.SESSION_ID,
 GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'herokukell' : process.env.GITHUB_USER_NAME,
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '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' : process.env.GITHUB_AUTH_TOKEN,        
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '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' : process.env.GITHUB_AUTH_TOKEN,        
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94784198085' : process.env.OWNER_NUMBER
 };
